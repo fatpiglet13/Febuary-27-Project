25979d85e0d7de7bd72e3e77b9634184065658a4
Vicki Arroyo <vicki11235813@gmail.com>
2024-02-28 21:18:58 +0000

    In Class XOR

--- XORinClassProject.py
+++ XORinClassProject.py
@@ -17,3 +17,45 @@ def decode(binary):
     charIndex = int(binary, 2)
     return characters[charIndex]
 
+def XOR(bit1, bit2):
+    if bit1 != bit2:
+        return "1"
+    else:
+         return "0"
+
+def XORonByte(byte, key):
+    emsg=" "
+    for i in range(len(byte)):
+        emsg += XOR(byte[i],key[i])
+    return emsg
+
+def XORonLetter(letter, keyLetter):
+    letterBin=encode(letter)
+    keyLetterBin=encode(keyletter)
+    encryptedLetter=XORonByte(letterBin, keyLetterBin)
+    return decode(encryptedLetter)
+
+def XORonSentance(sentance, key):
+    encryptedSentance= " "
+    genKey = generateKey(sentance, key)
+    for i in range(len(sentance)):
+        encryptedSentance += XORonLetter(sentance[i],key[i])
+    return encryptedSentance
+
+def generateKey(message, key):
+    if len(message) ==len(key):
+        return key
+    elif len(message) < len(key):
+        return key[0:len(message)]
+    else:
+        genKey = " "
+        rep= len(message)/len(key)
+        rem= len(message) % len(key)
+        for i in range(rep):
+            genKey += key
+        genKey += key[0:rem]
+        return genKey
+
+msg = input("Enter a message")
+key= input("Enter a key")
+print("Your encrypted message is:", XORonSentance(mesg,key))
